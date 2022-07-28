832dab8dac8928361a6424081847be9979303aab
ROPAN1991 <91415292+ROPAN1991@users.noreply.github.com>
2022-07-28 23:00:21 +0300

    hwgoit8

--- src/01-gallery.js
+++ src/01-gallery.js
@@ -1,24 +1,24 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items.js';
 
-const gallery = document.querySelector(".gallery");
+const gallery = document.querySelector('.gallery');
 
 const template = ({
-                      preview,
-                      original,
-                      description
-                  }) => `<a class="gallery__item" href="${original}">
+  preview,
+  original,
+  description,
+}) => `<a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
 </a>`;
 
-const render = (array) => {
-    const items = array.reduce((acc, item) => (acc += template(item)), '');
-    gallery.innerHTML = items;
+const render = array => {
+  const items = array.reduce((acc, item) => (acc += template(item)), '');
+  gallery.innerHTML = items;
 };
 
 render(galleryItems);
 
 new SimpleLightbox('.gallery a', {
-    captionDelay: 100
-})
+  captionDelay: 100,
+});
--- src/02-video.js
+++ src/02-video.js
@@ -1,33 +1,43 @@
-import Player from '@vimeo/player'
-import throttle from 'lodash.throttle'
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function() {
+player.on('play', function () {
   console.log('played the video!');
 });
 
-player.getVideoTitle().then(function(title) {
+player.getVideoTitle().then(function (title) {
   console.log('title:', title);
 });
 
-const setTimeIntoLocalStorage = throttle((currentTime) => window.localStorage.setItem('videoplayer-current-time', currentTime.seconds), 1000)
+const setTimeIntoLocalStorage = throttle(
+  currentTime =>
+    window.localStorage.setItem(
+      'videoplayer-current-time',
+      currentTime.seconds
+    ),
+  1000
+);
 
-player.on('timeupdate', function(currentTime) {
-  setTimeIntoLocalStorage(currentTime)
+player.on('timeupdate', function (currentTime) {
+  setTimeIntoLocalStorage(currentTime);
 });
 
-player.setCurrentTime(window.localStorage.getItem('videoplayer-current-time') || 0).then(function(seconds) {
-  // seconds = the actual time that the player seeked to
-}).catch(function(error) {
-  switch (error.name) {
-    case 'RangeError':
-      // the time was less than 0 or greater than the video’s duration
-      break;
+player
+  .setCurrentTime(window.localStorage.getItem('videoplayer-current-time') || 0)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
 
-    default:
-      // some other error occurred
-      break;
-  }
-});
+      default:
+        // some other error occurred
+        break;
+    }
+  });
